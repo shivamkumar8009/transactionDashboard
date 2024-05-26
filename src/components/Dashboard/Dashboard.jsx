a5514f1f7b80c66f5c3f8ced51ced515529e5426
Shivam <exceptionunique@gmail.com>
2024-05-26 11:36:28 +0530

    second table created

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -7,6 +7,27 @@ const Dashboard = () => {
   const [selectedMonth, setSelectedMonth] = useState("March");
   const [searchText, setSearchText] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetchData();
+  }, [selectedMonth]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://66346c589bb0df2359a17e03.mockapi.io/api/tasks`,
+        {
+          params: {
+            month: selectedMonth,
+          },
+        }
+      );
+      setData(response.data[0]);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   useEffect(() => {
     fetchTransactions();
@@ -71,77 +92,135 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="dashboard-container">
-      <h1>Transaction Details</h1>
-      <div className="controls">
-        <div className="month-dropdown">
-          <label htmlFor="month">Select Month:</label>
-          <select id="month" value={selectedMonth} onChange={handleMonthChange}>
-            <option value="January">January</option>
-            <option value="February">February</option>
-            <option value="March">March</option>
-            <option value="April">April</option>
-            <option value="May">May</option>
-            <option value="June">June</option>
-            <option value="July">July</option>
-            <option value="August">August</option>
-            <option value="September">September</option>
-            <option value="October">October</option>
-            <option value="November">November</option>
-            <option value="December">December</option>
-          </select>
+    <>
+      <div className="dashboard-container">
+        <h1>Transaction Details</h1>
+        <div className="controls">
+          <div className="month-dropdown">
+            <label htmlFor="month">Select Month:</label>
+            <select
+              id="month"
+              value={selectedMonth}
+              onChange={handleMonthChange}
+            >
+              <option value="January">January</option>
+              <option value="February">February</option>
+              <option value="March">March</option>
+              <option value="April">April</option>
+              <option value="May">May</option>
+              <option value="June">June</option>
+              <option value="July">July</option>
+              <option value="August">August</option>
+              <option value="September">September</option>
+              <option value="October">October</option>
+              <option value="November">November</option>
+              <option value="December">December</option>
+            </select>
+          </div>
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Search transaction..."
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+            <button onClick={clearSearch}>Clear</button>
+          </div>
         </div>
-        <div className="search">
-          <input
-            type="text"
-            placeholder="Search transaction..."
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-          />
-          <button onClick={handleSearch}>Search</button>
-          <button onClick={clearSearch}>Clear</button>
-        </div>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Category</th>
-            <th>Sold</th>
-            <th>Image</th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions.map((transaction) => (
-            <tr key={transaction.id}>
-              <td>{transaction.id}</td>
-              <td>{transaction.title}</td>
-              <td>{transaction.description}</td>
-              <td>{transaction.price}</td>
-              <td>{transaction.category}</td>
-              <td>{transaction.sold ? "Yes" : "No"}</td>
-              <td>
-                <img
-                  src={transaction.image}
-                  alt={transaction.title}
-                  className="transaction-image"
-                />
-              </td>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Category</th>
+              <th>Sold</th>
+              <th>Image</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className="pagination">
-        <button onClick={handlePrevPage} disabled={currentPage === 1}>
-          Previous
-        </button>
-        <span>{`Page ${currentPage}`}</span>
-        <button onClick={handleNextPage}>Next</button>
+          </thead>
+          <tbody>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td>{transaction.id}</td>
+                <td>{transaction.title}</td>
+                <td>{transaction.description}</td>
+                <td>{transaction.price}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.sold ? "Yes" : "No"}</td>
+                <td>
+                  <img
+                    src={transaction.image}
+                    alt={transaction.title}
+                    className="transaction-image"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="pagination">
+          <button onClick={handlePrevPage} disabled={currentPage === 1}>
+            Previous
+          </button>
+          <span>{`Page ${currentPage}`}</span>
+          <button onClick={handleNextPage}>Next</button>
+        </div>
       </div>
-    </div>
+      <div className="dashboard-container">
+        <h1>Monthly Sales Data</h1>
+        {/* <div className="controls">
+          <div className="month-dropdown">
+            <label htmlFor="month">Select Month:</label>
+            <select
+              id="month"
+              value={selectedMonth}
+              onChange={handleMonthChange}
+            >
+              <option value="January">January</option>
+              <option value="February">February</option>
+              <option value="March">March</option>
+              <option value="April">April</option>
+              <option value="May">May</option>
+              <option value="June">June</option>
+              <option value="July">July</option>
+              <option value="August">August</option>
+              <option value="September">September</option>
+              <option value="October">October</option>
+              <option value="November">November</option>
+              <option value="December">December</option>
+            </select>
+          </div>
+        </div> */}
+        {data ? (
+          <table className="data-table">
+            <thead>
+              <tr>
+                <th>Metric</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Total Sales</td>
+                <td>${data.totalSales}</td>
+              </tr>
+              <tr>
+                <td>Total Sold Items</td>
+                <td>{data.totalSoldItems}</td>
+              </tr>
+              <tr>
+                <td>Total Non-Sold Items</td>
+                <td>{data.totalNonSoldItems}</td>
+              </tr>
+            </tbody>
+          </table>
+        ) : (
+          <p>No data available for the selected month.</p>
+        )}
+      </div>
+    </>
   );
 };
 
